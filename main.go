37759d65675b0f2e3b742d58e04b801414b2b8ed
S1ro1 <matej.sirovatka@gmail.com>
2025-03-16 03:30:05 +0100

    Feat: works

--- main.go
+++ main.go
@@ -7,43 +7,12 @@ import (
 	"github.com/S1ro1/popcorn-cli/src/cmd"
 )
 
-func displayAsciiArt() {
-	art := `
- _   __                      _  ______          _   
-| | / /                     | | | ___ \        | |  
-| |/ /  ___ _ __ _ __   ___ | | | |_/ /  ___  _| |_ 
-|    \ / _ \ '__| '_ \ / _ \| | | ___ \ / _ \| | __|
-| |\  \  __/ |  | | | |  __/| | | |_/ /| (_) | | |_ 
-\_| \_/\___|_|  |_| |_|\___|_/ \____/ \___/|_|\__|
-                                                  
-    POPCORN CLI - GPU MODE
-    
- ┌───────────────────────────────────────┐
- │  ┌─────┐ ┌─────┐ ┌─────┐              │
- │  │ooOoo│ │ooOoo│ │ooOoo│              │▒
- │  │oOOOo│ │oOOOo│ │oOOOo│              │▒
- │  │ooOoo│ │ooOoo│ │ooOoo│   ┌────────┐ │▒
- │  └─────┘ └─────┘ └─────┘   │████████│ │▒
- │                            │████████│ │▒
- │ ┌────────────────────────┐ │████████│ │▒
- │ │                        │ │████████│ │▒
- │ │  POPCORN GPU COMPUTE   │ └────────┘ │▒
- │ │                        │            │▒
- │ └────────────────────────┘            │▒
- │                                       │▒
- └───────────────────────────────────────┘▒
-  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-    ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-`
-	fmt.Println(art)
-}
 
 func main() {
-	_, ok := os.LookupEnv("POPCORN_API_URL")
-	if !ok {
+	
+	if os.Getenv("POPCORN_API_URL") == "" {
 		fmt.Println("POPCORN_API_URL is not set. Please set it to the URL of the Popcorn API.")
 		os.Exit(1)
 	}
-	displayAsciiArt()
 	cmd.Execute()
 }
