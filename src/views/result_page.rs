627ad5d8d2c76d32d304b264d04f5e05f99239c3
S1ro1 <matej.sirovatka@gmail.com>
2025-06-06 02:57:02 +0200

    Feat: more cleanup

--- src/views/result_page.rs
+++ src/views/result_page.rs
@@ -1,33 +1,36 @@
 use crate::utils;
+use crossterm::event::{KeyCode, KeyEvent};
 use ratatui::{
     layout::{Alignment, Constraint, Layout, Rect},
+    prelude::Buffer,
     style::{Color, Style},
-    widgets::{Block, BorderType, Paragraph},
-    Frame,
+    widgets::{Block, BorderType, Paragraph, Widget},
 };
 
 pub struct ResultPage {
     result_text: Paragraph<'static>,
+    pub ack: bool,
 }
 
 impl ResultPage {
     pub fn new(result_text: String) -> Self {
         Self {
             result_text: Paragraph::new(result_text),
+            ack: false,
         }
     }
 
-    fn render_left(&self, frame: &mut Frame, left: Rect) {
+    fn render_left(&self, buf: &mut Buffer, left: Rect) {
         let left_block = Block::bordered()
             .border_type(BorderType::Plain)
             .border_style(Style::default().fg(Color::Yellow));
 
         let left_text = Paragraph::new(utils::get_ascii_art());
 
-        frame.render_widget(left_text.block(left_block), left);
+        left_text.block(left_block).render(left, buf);
     }
 
-    fn render_right(&self, frame: &mut Frame, right: Rect) {
+    fn render_right(&self, buf: &mut Buffer, right: Rect) {
         let right_block = Block::bordered()
             .border_type(BorderType::Plain)
             .border_style(Style::default().fg(Color::Yellow))
@@ -35,14 +38,23 @@ impl ResultPage {
             .title_style(Style::default().fg(Color::Red))
             .title_alignment(Alignment::Right);
 
-        frame.render_widget(self.result_text.clone().block(right_block), right);
+        let result_text = self.result_text.clone().block(right_block);
+        result_text.render(right, buf);
     }
 
-    pub fn render(&self, frame: &mut Frame) {
-        let layout = Layout::horizontal([Constraint::Percentage(45), Constraint::Percentage(55)]);
-        let [left, right] = layout.areas(frame.size());
+    pub fn handle_key_event(&mut self, key: KeyEvent) {
+        if key.code == KeyCode::Char('q') {
+            self.ack = true;
+        }
+    }
+}
 
-        self.render_left(frame, left);
-        self.render_right(frame, right);
+impl Widget for &ResultPage {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let layout = Layout::horizontal([Constraint::Percentage(45), Constraint::Percentage(55)]);
+        let [left, right] = layout.areas(area);
+
+        self.render_left(buf, left);
+        self.render_right(buf, right);
     }
 }
