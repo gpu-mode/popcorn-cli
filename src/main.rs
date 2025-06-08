3d0ae364cc7e3378de1daa17170503a4fb80bf41
Mark Saroufim <marksaroufim@gmail.com>
2025-06-08 13:13:35 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,11 @@ use std::process;
 
 #[tokio::main]
 async fn main() {
+    // Set the API URL FIRST - before anything else
+    env::set_var("POPCORN_API_URL", "https://discord-cluster-manager-1f6c4782e60a.herokuapp.com");
+    
     // Parse command line arguments
     let cli = Cli::parse();
-
-    // Popcorn API URL check (needed for most commands)
-    // For hackathon: hardcode the API URL if not set
-    if env::var("POPCORN_API_URL").is_err() {
-        env::set_var("POPCORN_API_URL", "https://discord-cluster-manager-1f6c4782e60a.herokuapp.com");
-    }
 
     // Execute the parsed command
     if let Err(e) = cmd::execute(cli).await {
